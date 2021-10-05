c71db4e3eaa61943c6152261df092e5099c62570
Nii Kunihiro <nii.kunihiro@gmail.com>
2021-10-06 02:02:42 +0900

    add: service.getTodos のシンプルテストを追加

--- todo_api/src/todo/todo.service.spec.ts
+++ todo_api/src/todo/todo.service.spec.ts
@@ -12,6 +12,21 @@ describe('TodoService', () => {
     transaction: jest.fn(),
   });
 
+  const mockTodo: Todo = {
+    id: 1,
+    title: 'hello my first todo',
+    content: 'TODOの登録ですよー',
+    created_at: '2021-10-05T15:28:58.000Z',
+    updated_at: '2021-10-05T15:28:58.000Z',
+  };
+
+  let mockTodos = [
+    mockTodo,
+    { ...mockTodo, id: 2 },
+    { ...mockTodo, id: 3 },
+    { ...mockTodo, id: 4 },
+  ];
+
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       providers: [
@@ -20,7 +35,7 @@ describe('TodoService', () => {
         {
           provide: getRepositoryToken(Todo),
           useValue: {
-            find: () => {},
+            find: () => mockTodos,
           },
         },
         {
@@ -36,5 +51,10 @@ describe('TodoService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should test getTodos', async () => {
+    const todos = await service.getTodos();
+    expect(mockTodos).toEqual(todos);
   });
 });
