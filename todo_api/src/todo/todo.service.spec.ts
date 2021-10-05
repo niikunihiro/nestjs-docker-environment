892a492b60348873edf786904cbc75e4cb6c1559
Nii Kunihiro <nii.kunihiro@gmail.com>
2021-10-06 01:35:17 +0900

    update: service の spec テストをとりあえず通るように変更

--- todo_api/src/todo/todo.service.spec.ts
+++ todo_api/src/todo/todo.service.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Todo } from '../entity/todo.entity';
+import { Connection } from 'typeorm';
+import { NowProvider } from '../date/now.provider';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let connection;
+  const mockConnection = () => ({
+    transaction: jest.fn(),
+  });
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        TodoService,
+        NowProvider,
+        {
+          provide: getRepositoryToken(Todo),
+          useValue: {
+            find: () => {},
+          },
+        },
+        {
+          provide: Connection,
+          useFactory: mockConnection,
+        },
+      ],
     }).compile();
 
-    service = module.get<TodoService>(TodoService);
+    service = await moduleRef.get<TodoService>(TodoService);
+    connection = await moduleRef.get<Connection>(Connection);
   });
 
   it('should be defined', () => {
