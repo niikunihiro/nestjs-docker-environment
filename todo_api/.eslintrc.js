5b90fe84d437cabfba3d7d85dfb6a0abeede332f
Nii Kunihiro <nii.kunihiro@gmail.com>
2021-10-14 00:24:15 +0900

    update: eslint, prettier の設定変更
    semi: true を追加
    合わせて eslint の設定を削除

--- todo_api/.eslintrc.js
+++ todo_api/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
