424980f5e5c853e618bcc9f3b0acdd7afab74eef
Nii Kunihiro <nii.kunihiro@gmail.com>
2021-11-07 13:31:45 +0900

    add: e2e test
    実際にDBへ接続するテストになっている

--- todo_api/test/app.e2e-spec.ts
+++ todo_api/test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateTodoDto } from 'src/todo/dto/create-todo.dto';
+import { Todo } from 'src/entity/todo.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +17,49 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  // 全件取得のための関数
+  const getTodoAll = async (): Promise<Array<Todo>> => {
+    const res = await request(app.getHttpServer()).get('/todo');
+    expect(res.status).toEqual(200);
+
+    return res.body as Array<Todo>;
+  };
+
+  describe('TodoController (e2e)', () => {
+    describe('Create API of Todo', () => {
+      it('/todo (POST)', async () => {
+        // 登録前のデータ数を持っとく
+        let todoAllResponse = await getTodoAll();
+        const beforeRows = todoAllResponse.length;
+
+        const body: CreateTodoDto = {
+          title: 'hello my todo',
+          content: 'TODOの登録ですよ',
+        };
+
+        // 新規登録
+        const res = await request(app.getHttpServer()).post('/todo').send(body);
+        expect(res.status).toEqual(201);
+
+        const todoResponse = res.body as Todo;
+        expect(todoResponse.title).toEqual(body.title);
+
+        // 登録後のデータ数がインクリメントされてるか確認
+        todoAllResponse = await getTodoAll();
+        expect(todoAllResponse.length).toEqual(beforeRows + 1);
+
+        // レスポンスの ID を使って GET /todo/:id
+        const resGet = await request(app.getHttpServer()).get(
+          '/todo/' + todoResponse.id.toString(),
+        );
+        expect(resGet.status).toEqual(200);
+        const todoGetResponse = resGet.body as Todo;
+        expect(todoGetResponse.title).toEqual(body.title);
+      });
+    });
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
